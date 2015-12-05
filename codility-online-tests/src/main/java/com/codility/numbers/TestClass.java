e3fbc5c42527b7e2fa529abb2a80c012264d951e
mohamedsaligh <mohamed.saligh@gmail.com>
2015-12-05 14:14:29 +0800

    codility tests
    standalone TestClass

---
+++ codility-online-tests/src/main/java/com/codility/numbers/TestClass.java
@@ -0,0 +1,11 @@
+package com.codility.numbers;
+
+/**
+ * Created by saligh on 5/12/15.
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("Test Class!");
+    }
+}
